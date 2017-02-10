15474f047348c3dcd4f29a4d269a75963879a6e2
micktusker <micktusker@gmail.com>
2017-02-10 16:20:05 +0000

    Script to transfer a table from SQLite to PostgreSQL.

---
+++ transfer_tables_sqlite_to_pg.R
@@ -0,0 +1,37 @@
+# Defines a function that transfers a table from SQLite to PostgreSQL via an R data frame.
+# 
+# Steps:
+# Connect to SQLite and PostgreSQL
+# Pass the SQLite and PostgreSQL connection and the SQLite table name to a function.
+# Call the function then does the actual transfer.
+
+# Assumes:
+# The ssh tunnel is set up to the Tusk Redhat Pg database.
+
+# This works surprisingly well and can transfer tables with >1.5M rows quite quickly.
+library(DBI)
+library(RSQLite)
+library(RPostgreSQL)
+SQLITE_DB_NAME<- 'tcga_mutation.db'
+SQLITE_PARENT_DIR <- '~/TCGA/tcga_mutation_data'
+SQLITE_DB_PATH <- file.path(SQLITE_PARENT_DIR, SQLITE_DB_NAME)
+# PostgreSQL connection parameters
+
+pg.host <- 'localhost'
+pg.user <- 'micktusker'
+pg.port <- 5433
+pg.dbname <- 'cd38'
+sqlite.drv <- dbDriver("SQLite")
+sqlite.conn <- dbConnect(sqlite.drv, SQLITE_DB_PATH)
+pg.drv <- dbDriver("PostgreSQL")
+pg.conn <- dbConnect(pg.drv, user = pg.user, port = pg.port, host = pg.host, dbname = pg.dbname)
+
+transfer.table <- function(table.name) {
+  table.as.df <- dbReadTable(sqlite.conn, table.name)
+  dbWriteTable(pg.conn, table.name, table.as.df, row.names=FALSE)
+}
+#transfer.table('directories')
+#transfer.table('mutation_files')
+transfer.table('subject_mutations')
+dbDisconnect(sqlite.conn)
+dbDisconnect(pg.conn)
