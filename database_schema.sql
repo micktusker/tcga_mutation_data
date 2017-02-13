11321ba4c9720d3ee133679ea8499bd026a9e7c3
micktusker <micktusker@gmail.com>
2017-02-13 15:49:46 +0000

    Added a UDF to get the individual mRNA expression data for a given gene, cancer code and participant.

--- database_schema.sql
+++ database_schema.sql
@@ -97,3 +97,63 @@ WHERE
   
 COMMENT ON MATERIALIZED VIEW expression.tcga_mrna_participants_processed IS 'In this view the barcodes are split into their individual parts to allow joining with other data types. To be used to match missense mutations with mRNA expression.';
 CREATE INDEX tcga_mrna_participants_processed_pcpt_idx ON expression.tcga_mrna_participants_processed(participant);
+
+-- Get mRNA expression data for participant-gene-cancer combination.
+
+CREATE OR REPLACE FUNCTION user_defined_functions.get_mrna_expression_for_participant_gene_cancer(p_participant TEXT, p_gene_name TEXT, p_tcga_cancer_code TEXT)
+RETURNS REAL
+AS
+$$
+DECLARE
+  l_sample_index INTEGER;
+  l_expression_value INTEGER;
+BEGIN
+  SELECT
+    sample_index INTO l_sample_index
+  FROM
+    expression.tcga_mrna_participants_processed
+  WHERE
+    participant = p_participant
+    AND
+      tcga_cancer_code = p_tcga_cancer_code;
+  SELECT
+    tmnm.expression_values[l_sample_index] INTO l_expression_value
+  FROM
+    expression.tcga_mrna_normalized_matrices tmnm
+    JOIN
+      expression.tcga_file_cancer_code_lookup tcga_lu ON tmnm.file_code = tcga_lu.file_code
+  WHERE
+    tmnm.gene_name = p_gene_name
+    AND
+      tcga_lu.tcga_cancer_code = p_tcga_cancer_code;
+  RETURN l_expression_value;
+END;
+$$
+LANGUAGE plpgsql;
+COMMENT ON FUNCTION user_defined_functions.get_mrna_expression_for_participant_gene_cancer(TEXT, TEXT, TEXT)
+IS 'Given a participant name (in MV expression.tcga_mrna_participants_processed), an uppercase gene name that must appear in table "expression.tcga_mrna_normalized_matrices", and a recognized TCGA cancer code, return the indivual mRNA expression value for that combination of arguments. Example call: SELECT user_defined_functions.get_mrna_expression_for_participant_gene_cancer("A5JA", "CD38", "ACC")';
+
+CREATE TABLE expression.tcga_participant_mrna_expr_vals(
+  tcga_participant_mrna_expr_vals_id SERIAL PRIMARY KEY,
+  participant TEXT,
+  tcga_cancer_code TEXT,
+  sample_index INTEGER,
+  gene_name TEXT,
+  expression_value REAL
+);
+COMMENT ON TABLE expression.tcga_participant_mrna_expr_vals IS
+'Contains the indivual mRNA expression levels for a given gene-cancer combination. Used to investigate potential relationship between expression of specific genes (PD-1 and PD-L1 specifically) and the number of missense (potential neo-antigens) in that sample.';
+
+INSERT INTO expression.tcga_participant_mrna_expr_vals(participant, tcga_cancer_code, sample_index, gene_name, expression_value)
+SELECT
+  participant,
+  tcga_cancer_code,
+  sample_index,
+  'PDCD1'::TEXT gene_name,
+  user_defined_functions.get_mrna_expression_for_participant_gene_cancer(participant, 'PDCD1', tcga_cancer_code) expression_value
+FROM
+  expression.tcga_mrna_participants_processed;
+
+
+
+
